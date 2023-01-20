ad44efca5204ed091c6dcc1d79482585ef697946
td-tan <47648230+td-tan@users.noreply.github.com>
2023-01-20 17:51:17 +0100

    refactor: extract method

--- sketch.js
+++ sketch.js
@@ -1,14 +1,19 @@
-const sketchpad = document.querySelector('.sketchpad');
 
-for (let i = 0; i < 16; i++) {
-    const outer_div = document.createElement('div');
-    for(let j = 0; j < 16; j++) {
-        const inner_div = document.createElement('div');
-        inner_div.classList.add('square');
-        outer_div.appendChild(inner_div);
+function createSketchpad(n, m) {
+    const sketchpad = document.querySelector('.sketchpad');
+
+    for (let i = 0; i < n; i++) {
+        const outer_div = document.createElement('div');
+        for(let j = 0; j < m; j++) {
+            const inner_div = document.createElement('div');
+            inner_div.classList.add('square');
+            outer_div.appendChild(inner_div);
+        }
+        sketchpad.appendChild(outer_div);
     }
-    sketchpad.appendChild(outer_div);
 }
+
+createSketchpad(16, 16);
 
 const squares = document.querySelectorAll('.square');
 
